8456ade96671b81e73cb49a6bd102c00787381c8
Steve <lorenz21@hawaii.edu>
2018-09-27 15:59:18 -1000

    Checkpoint 5 - Basic Node App: complete

---
+++ calculator.js
@@ -0,0 +1,9 @@
+exports.add = function(x, y) {
+    let result = x + y;
+    console.log(result);
+}
+
+exports.multiply = function(x, y) {
+    let result = x * y;
+    console.log(result);
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+const greetings = require("./greeting.js");
+const calculator = require("./calculator.js");
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+calculator.add(2, 5);
+calculator.multiply(6, 6);
